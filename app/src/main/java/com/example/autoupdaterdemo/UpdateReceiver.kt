53d7618a974b09b3e3e25f11d6bcf6a4f29043a8
Constantine Mars <constantine.mars@gmail.com>
2020-03-09 01:03:18 -0700

    Receiver update

--- app/src/main/java/com/example/autoupdaterdemo/UpdateReceiver.kt
+++ app/src/main/java/com/example/autoupdaterdemo/UpdateReceiver.kt
@@ -1,16 +1,26 @@
 package com.example.autoupdaterdemo
 
+import android.annotation.SuppressLint
 import android.content.BroadcastReceiver
 import android.content.Context
 import android.content.Intent
+import android.util.Log
+import android.widget.Toast
+import timber.log.Timber
 
 class UpdateReceiver : BroadcastReceiver() {
+    @SuppressLint("UnsafeProtectedBroadcastReceiver")
+    override fun onReceive(context: Context?, intent: Intent?) {
+        Timber.e("update broadcast received")
 
-    override fun onReceive(context: Context, intent: Intent) {
+        if (context == null) {
+            return
+        }
 
-        // Restart your app here
         val i = Intent(context, MainActivity::class.java)
         i.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK)
         context.startActivity(i)
+
+        Toast.makeText(context, "Updated to version #${BuildConfig.VERSION_CODE}!", Toast.LENGTH_LONG).show()
     }
 }
